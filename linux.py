addedc041b3ff674b478f4c445420ed44db42434
Aditya <aditya@adityapatawari.com>
2013-10-03 16:41:58 +0530

    adding some basic linux facts for IP, memory and cpu

---
+++ linux.py
@@ -0,0 +1,25 @@
+import re
+import subprocess
+
+def ipaddress():
+  p = subprocess.Popen(["/sbin/ip","addr","show"], stdout=subprocess.PIPE)
+  ip=p.communicate()
+  pattern = re.compile(r'inet\s*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
+  return pattern.findall(ifconfig[0])
+
+def fqdn():
+  return socket.gethostname()
+
+def memory():
+  return subprocess.Popen(["grep","MemTotal","/proc/meminfo"], stdout=subprocess.PIPE).communicate()[0].split()[1:]
+
+def swap():
+  return subprocess.Popen(["grep","SwapTotal","/proc/meminfo"], stdout=subprocess.PIPE).communicate()[0].split()[1:]
+
+def cpu_cores():
+  return subprocess.Popen(["grep","-m1","cores","/proc/cpuinfo"], stdout=subprocess.PIPE).communicate()[0].split()[-1]
+
+def cpu_threads():
+  return subprocess.Popen(["grep","-c","processor","/proc/cpuinfo"], stdout=subprocess.PIPE).communicate()[0].split()[-1]
+
+
