dda78ddf502fd5ba0fb4d5fc20852344cb677c39
Aditya <aditya@adityapatawari.com>
2013-10-13 14:53:08 +0530

    posting to a url

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
 import json 
 import platform
 from types import ModuleType
-import sys 
+import sys
+import urllib
+import urllib2
 
 if platform.system().lower() == 'linux':
   import linux as FactsLib
@@ -15,5 +17,12 @@ facts={}
 for method in dir(FactsLib):
   if method[0] != '_' and type(getattr(FactsLib,method)) is not ModuleType:
     facts[str(method)] = getattr(FactsLib,method)()
-
 print facts
+if sys.argv[1]:
+  url = sys.argv[1]
+  tmp=json.dumps(facts)
+  print tmp
+  facts_json = urllib.urlencode(facts)
+  req = urllib2.Request(url, facts_json)
+  response = urllib2.urlopen(req)
+  the_page = response.read()
