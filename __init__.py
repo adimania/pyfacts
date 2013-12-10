6b1a03738003bda7c680e93f97850a72a48e0816
Aditya <aditya@adityapatawari.com>
2013-12-10 12:45:28 +0530

    putting the post in a try block

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ import urllib2
 if platform.system().lower() == 'linux':
   import linux as FactsLib
 elif platform.system().lower() == 'darwin':
-  import mac as FactsLib
+  import osx as FactsLib
 else:
   print "OS not supprted"
   sys.exit(0)
@@ -18,11 +18,15 @@ for method in dir(FactsLib):
   if method[0] != '_' and type(getattr(FactsLib,method)) is not ModuleType:
     facts[str(method)] = getattr(FactsLib,method)()
 print facts
-if sys.argv[1]:
-  url = sys.argv[1]
-  tmp=json.dumps(facts)
-  print tmp
-  facts_json = urllib.urlencode(facts)
-  req = urllib2.Request(url, facts_json)
-  response = urllib2.urlopen(req)
-  the_page = response.read()
+
+try:
+  if sys.argv[1]:
+    url = sys.argv[1]
+    tmp=json.dumps(facts)
+    print tmp
+    facts_json = urllib.urlencode(facts)
+    req = urllib2.Request(url, facts_json)
+    response = urllib2.urlopen(req)
+    the_page = response.read()
+except:
+  pass
